2a4277389ff4c81aa043ee6abdfde3cf0aebb929
Derek King <dking@fetchrobotics.com>
2016-02-22 16:59:55 -0800

    Use normalize_angle instead of the roll-your-own one from amcl.

--- amcl/src/amcl/sensors/amcl_odom.cpp
+++ amcl/src/amcl/sensors/amcl_odom.cpp
@@ -34,27 +34,14 @@
 
 #include "amcl_odom.h"
 
+#include "angles/angles.h"
+
 using namespace amcl;
 
 static double
-normalize(double z)
-{
-  return atan2(sin(z),cos(z));
-}
-static double
 angle_diff(double a, double b)
 {
-  double d1, d2;
-  a = normalize(a);
-  b = normalize(b);
-  d1 = a-b;
-  d2 = 2*M_PI - fabs(d1);
-  if(d1 > 0)
-    d2 *= -1.0;
-  if(fabs(d1) < fabs(d2))
-    return(d1);
-  else
-    return(d2);
+  return angles::normalize_angle(a-b);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
